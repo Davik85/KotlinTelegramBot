541ddbf7649d130d51d53ed76983ddafd49de15c
David <Davik50@gmail.com>
2025-07-11 08:15:38 +0200

    KTB-16-Menu

--- src/main/kotlin/Dictionary/Telegram.kt
+++ src/main/kotlin/Dictionary/Telegram.kt
@@ -3,6 +3,7 @@ package org.example.Dictionary
 const val UPDATE_ID_PATTERN = """"update_id":\s*(\d+)"""
 const val CHAT_ID_PATTERN = """"chat":\s*\{[^}]*"id":\s*(-?\d+)"""
 const val TEXT_PATTERN = """"text":"(.+?)""""
+const val DATA_PATTERN = """"data":"(.+?)""""
 
 fun main(args: Array<String>) {
     val botToken = args[0]
@@ -11,6 +12,7 @@ fun main(args: Array<String>) {
     val updateIdRegex = UPDATE_ID_PATTERN.toRegex()
     val chatIdRegex = CHAT_ID_PATTERN.toRegex()
     val textRegex = TEXT_PATTERN.toRegex()
+    val dataRegex = DATA_PATTERN.toRegex()
 
     val botService = TelegramBotService(botToken)
 
@@ -20,18 +22,34 @@ fun main(args: Array<String>) {
         println(updates)
 
         val updateIdMatch = updateIdRegex.find(updates)
+        if (updateIdMatch != null) {
+            updateId = updateIdMatch.groupValues[1].toInt() + 1
+        }
+
         val chatIdMatch = chatIdRegex.find(updates)
+        val chatId = chatIdMatch?.groupValues?.get(1)?.toLongOrNull()
         val textMatch = textRegex.find(updates)
+        val text = textMatch?.groupValues?.get(1)
+        val dataMatch = dataRegex.find(updates)
+        val data = dataMatch?.groupValues?.get(1)
 
-        if (updateIdMatch == null || chatIdMatch == null || textMatch == null) continue
+        if (text != null && chatId != null) {
+            println("Получено сообщение: $text (chatId = $chatId)")
+            when {
+                text.equals("hello", ignoreCase = true) -> {
+                    botService.sendMessage(chatId, "Hello")
+                }
+                text.equals("/menu", ignoreCase = true) || text.equals("menu", ignoreCase = true) -> {
+                    botService.sendMenu(chatId)
+                }
+            }
+        }
 
-        updateId = updateIdMatch.groupValues[1].toInt() + 1
-        val chatId = chatIdMatch.groupValues[1].toLong()
-        val text = textMatch.groupValues[1]
-        println("Получено сообщение: $text (chatId = $chatId)")
-
-        if (text.equals("hello", ignoreCase = true)) {
-            botService.sendMessage(chatId, "Hello")
+        if (data != null && chatId != null) {
+            when (data.lowercase()) {
+                "statistics_clicked" -> botService.sendMessage(chatId, "Выучено 10 из 10 слов | 100%")
+                "learn_words_clicked" -> botService.sendMessage(chatId, "Давай начнем изучение новых слов!")
+            }
         }
     }
 }
